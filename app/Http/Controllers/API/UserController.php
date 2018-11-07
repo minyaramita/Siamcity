9b743db3257c647de14c02d7f04f690c7627083a
minyaramita <minyaramita@gmail.com>
2018-11-07 12:31:18 +0700

    EP.33 Upload and Convert Base64 String to Image In PHP and Laravel

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -57,6 +57,13 @@ class UserController extends Controller
     public function updateProfile(Request $request)
     {
         $user =  auth('api')->user();
+
+        if($request->photo){
+
+            $name = time().'.' . explode('/', explode(':', substr($request->photo, 0, strpos($request->photo, ';')))[1])[1];
+
+            \Image::make($request->photo)->save(public_path('img/profile/').$name);
+        }
         return $request->photo;
         // return ['message' => "Success"];
     }
